612cf230714c472eb0bd2cda943bc7ae8625696d
pavanyendluri588 <pavanyendluri588@gmail.com>
2023-04-14 18:54:46 +0530

    april_2nd_upload

---
+++ INT232_DATA SCIENCE TOOLBOX  R PROGRAMMING/r_code/math_functions.R
@@ -0,0 +1,27 @@
+min()
+max()
+sqrt()
+abs()
+ceiling()
+floor()
+round()
+log()
+log10()
+mod()
+exp()#exponential
+mean()
+median()
+
+
+sin()
+cos()
+tan()
+asin()  #inverse of sin
+acos()
+atan()
+
+sinh()  #hyperbolic sin 
+cosh()
+tanh()
+
+
